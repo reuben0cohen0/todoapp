7d42154eac472acd577af1b1278b8b914af16f63
reuben0cohen0 <39714643+reuben0cohen0@users.noreply.github.com>
2020-04-20 14:38:49 +1200

    Add files via upload

---
+++ src/TodoItems.js
@@ -0,0 +1,39 @@
+import React, {
+    Component
+} from "react";
+
+class TodoItems extends Component {
+    constructor(props) {
+        super(props);
+
+        this.createTasks = this.createTasks.bind(this);
+    }
+    delete(key) {
+        this.props.delete(key);
+    }
+    //list functions; delte, colour link
+    createTasks(item) {
+        return <li onClick = {
+            () => this.delete(item.key)
+        }
+        key = {
+            item.key
+        } > {
+            item.text
+        } < /li>
+    }
+
+    render() {
+        var todoEntries = this.props.entries;
+        var listItems = todoEntries.map(this.createTasks);
+        //list here
+        return ( <
+            ul className = "theList" > {
+                listItems
+            } <
+            /ul>
+        );
+    }
+};
+
+export default TodoItems;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import TodoList from "./TodoList";
+
+var destination = document.querySelector("#container")
+//import todo list code
+ReactDOM.render( <
+    div >
+    <
+    TodoList / >
+    <
+    /div>,
+    destination
+);
